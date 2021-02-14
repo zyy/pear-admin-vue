77f4a009bf8cf6c5dd3a317e2bd9e909667d6d42
就眠仪式 <854085467@qq.com>
2021-02-15 00:12:29 +0800

    个人设置

--- src/views/account/settings/index.vue
+++ src/views/account/settings/index.vue
@@ -1,11 +1,28 @@
 <template>
-  <page-container></page-container>
+  <page-container>
+    <a-card>
+      <a-tabs v-model:activeKey="activeKey" :tab-position="tabPosition">
+      <a-tab-pane key="1" tab="基本设置">Content of Tab 1</a-tab-pane>
+      <a-tab-pane key="2" tab="安全设置">Content of Tab 2</a-tab-pane>
+      <a-tab-pane key="3" tab="账号绑定">Content of Tab 3</a-tab-pane>
+      <a-tab-pane key="4" tab="消息通知">Content of Tab 3</a-tab-pane>
+      </a-tabs>
+    </a-card>
+  </page-container>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 export default defineComponent({
-  name: 'index'
+  name: 'index',
+  setup () {
+    const tabPosition = ref('left')
+    const activeKey = ref('1')
+    return {
+      tabPosition,
+      activeKey
+    }
+  }
 })
 </script>
 
