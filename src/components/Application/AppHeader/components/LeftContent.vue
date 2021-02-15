18e04f7037a526bdf7bb620698c9ff7602897018
就眠仪式 <854085467@qq.com>
2021-02-15 10:14:54 +0800

    清空头部左侧内容

--- src/components/Application/AppHeader/components/LeftContent.vue
+++ src/components/Application/AppHeader/components/LeftContent.vue
@@ -1,21 +1,13 @@
 <template>
-  <div class="item">
-      <MenuFoldOutlined></MenuFoldOutlined>
-  </div>
-  <div class="item">
-      <RedoOutlined></RedoOutlined>
-  </div>
+  <div></div>
 </template>
 
 <script lang="ts">
-import { MenuFoldOutlined, RedoOutlined } from '@ant-design/icons-vue'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'LeftContent',
   components: {
-    MenuFoldOutlined,
-    RedoOutlined
   }
 })
 </script>
