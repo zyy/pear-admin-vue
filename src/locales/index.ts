8f618e846b5b049c3774d6f271efd0b7986c6980
Jobin <425605679@qq.com>
2021-02-22 21:27:15 +0800

    fix: bugs

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,9 +1,9 @@
 import app from '@/app'
 import { loadLocaleMessages, setupI18n } from './i18n'
-import store from '@/store'
+import config from '@/config/pear.config'
+import storage from 'store'
 
-const DEFAULT_LANG = store.getters['app/language']
-
+const DEFAULT_LANG = storage.get('pear_language') || config.defaultLanguage
 const i18n = setupI18n({
   globalInjection: true,
   legacy: false,
@@ -13,9 +13,9 @@ const i18n = setupI18n({
   }
 })
 
+app.use(i18n)
 loadLocaleMessages(i18n, DEFAULT_LANG).then(() => {
   console.log('init i18n')
 })
-app.use(i18n)
 
 export { i18n }
